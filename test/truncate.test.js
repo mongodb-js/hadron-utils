dd9df139eeb15cd591cad75eef17730ddf95b089
Durran Jordan <durran@gmail.com>
2016-10-07 11:34:45 -0400

    Creating hadron-utils:
    - Added truncate function: require('hadron-utils').truncate

---
+++ test/truncate.test.js
@@ -0,0 +1,28 @@
+const expect = require('chai').expect;
+const truncate = require('../index').truncate;
+
+describe('.truncate', function() {
+  context('when the string length is less than the max length', function() {
+    const value = truncate('test', 100);
+
+    it('returns the string', function() {
+      expect(value).to.equal('test');
+    });
+  });
+
+  context('when the string length is greater than the max length', function() {
+    const value = truncate('test', 2);
+
+    it('returns the truncated string plus an ellipsis', function() {
+      expect(value).to.equal('te...');
+    });
+  });
+
+  context('when the string length is equal to the max length', function() {
+    const value = truncate('test', 4);
+
+    it('returns the string', function() {
+      expect(value).to.equal('test');
+    });
+  });
+});
